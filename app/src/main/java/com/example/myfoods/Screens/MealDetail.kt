aeadd1be457b2f16a216f3f4d3a9a83e97c7b2a3
RehanGith <chrehan1007@gmail.com>
2025-02-16 08:33:58 +0500

    Added fragment Meal Detail

---
+++ app/src/main/java/com/example/myfoods/Screens/MealDetail.kt
@@ -0,0 +1,13 @@
+package com.example.myfoods.Screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myfoods.R
+
+
+class MealDetail : Fragment(R.layout.activity_meal_detailes) {
+
+}

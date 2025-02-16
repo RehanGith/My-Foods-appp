356792856400ad06129c195bc7553474b1ed3622
RehanGith <chrehan1007@gmail.com>
2025-02-16 16:01:25 +0500

    Created logic for Detailed fragment

--- app/src/main/java/com/example/myfoods/Screens/MealDetail/MealDetail.kt
+++ app/src/main/java/com/example/myfoods/Screens/MealDetail/MealDetail.kt
@@ -1,31 +1,74 @@
 package com.example.myfoods.Screens.MealDetail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.myfoods.Model.Meal
 import com.example.myfoods.R
 import com.example.myfoods.databinding.ActivityMealDetailesBinding
 
 
 class MealDetail : Fragment(R.layout.activity_meal_detailes) {
-    lateinit var binding: ActivityMealDetailesBinding
-
+    private lateinit var binding: ActivityMealDetailesBinding
+    private lateinit var dtMeal:Meal
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = ActivityMealDetailesBinding.bind(view)
+        showLoading()
         var myMeal : Meal? = null
         val meal = arguments?.getSerializable("meal") as Meal?
         if (meal != null) {
             myMeal = meal
-            updateUI(myMeal)
+            stopLoading()
+            setTextsInViews(myMeal)
         }
     }
 
-    private fun updateUI(meal: Meal) {
+    private fun setTextsInViews(meal: Meal) {
+        this.dtMeal = meal
+        binding.apply {
+            tvInstructions.text = getString(R.string.instructions)
+            tvContent.text = meal.strInstructions
+            tvAreaInfo.visibility = View.VISIBLE
+            tvCategoryInfo.visibility = View.VISIBLE
+            tvAreaInfo.text = getString(R.string.AreaInfo, tvAreaInfo.text, meal.strArea)
+            tvCategoryInfo.text =
+                getString(R.string.catagory_info, tvCategoryInfo.text, meal.strCategory)
+            imgYoutube.visibility = View.VISIBLE
+            imgYoutube.setOnClickListener {
+                val ytUrl = meal.strYoutube
+                if (ytUrl != null) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(ytUrl))
+                    startActivity(intent)
+                }
+            }
+            collapsingToolbar.title = meal.strMeal
+            Glide.with(this@MealDetail)
+                .load(meal.strMealThumb ?: R.drawable.ic_launcher_foreground)
+                .into(imgMealDetail)
+
+        }
+    }
+
+
+    private fun showLoading() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnSave.visibility = View.GONE
+        binding.imgYoutube.visibility = View.INVISIBLE
+    }
+
+
+    private fun stopLoading() {
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnSave.visibility = View.VISIBLE
+
+        binding.imgYoutube.visibility = View.VISIBLE
 
     }
 

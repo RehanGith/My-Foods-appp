36b6f6fb1547a04e3df42089fca0e39d43d383d5
RehanGith <chrehan1007@gmail.com>
2025-02-16 08:30:01 +0500

    Added category Detail fragment

---
+++ app/src/main/java/com/example/myfoods/Screens/CategoryDetail.kt
@@ -0,0 +1,9 @@
+package com.example.myfoods.Screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myfoods.R
+
+class CategoryDetail : Fragment(R.layout.activity_categories) {
+
+}

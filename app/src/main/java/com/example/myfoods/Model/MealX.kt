390d52e95ed190e404d671b33dc01d2d9e3e1d7a
RehanGith <chrehan1007@gmail.com>
2025-02-16 17:43:46 +0500

    Created Model Classes for Popular

---
+++ app/src/main/java/com/example/myfoods/Model/MealX.kt
@@ -0,0 +1,7 @@
+package com.example.myfoods.Model
+
+data class MealX(
+    val idMeal: String,
+    val strMeal: String,
+    val strMealThumb: String
+)
---
+++ app/src/main/java/com/example/myfoods/Model/PopularMeal.kt
@@ -0,0 +1,5 @@
+package com.example.myfoods.Model
+
+data class PopularMeal(
+    val meals: List<MealX>
+)

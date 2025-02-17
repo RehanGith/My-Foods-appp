f475721e4ad79ff9e0b266ad8cd90e463aeaa5cf
RehanGith <chrehan1007@gmail.com>
2025-02-17 17:56:52 +0500

    created adapter for faovorite MealDB

---
+++ app/src/main/java/com/example/myfoods/Screens/Favorites/FavoritesViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myfoods.Screens.Favorites
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class FavoritesViewModel(app : Application): AndroidViewModel(app) {
+}

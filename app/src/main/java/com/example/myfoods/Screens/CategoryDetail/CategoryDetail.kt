7168b310cffa543d0c42c2644d5827b7c68e0f76
RehanGith <chrehan1007@gmail.com>
2025-02-17 11:26:38 +0500

    successfully implemented the display catagory list

--- app/src/main/java/com/example/myfoods/Screens/CategoryDetail/CategoryDetail.kt
+++ app/src/main/java/com/example/myfoods/Screens/CategoryDetail/CategoryDetail.kt
@@ -7,14 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.myfoods.Adapter.CategoryAdapter
+import com.example.myfoods.Adapter.MealAdapter
+import com.example.myfoods.Model.Category
+import com.example.myfoods.Model.MealX
 import com.example.myfoods.R
 import com.example.myfoods.Screens.Home.HomeViewModel
 import com.example.myfoods.databinding.ActivityCategoriesBinding
 
-class CategoryDetail : Fragment(R.layout.activity_categories) {
+class CategoryDetail : Fragment(R.layout.activity_categories), MealAdapter.OnItemViewClick {
     private lateinit var binding: ActivityCategoriesBinding
     private lateinit var viewModel: CategoryViewModel
     private lateinit var viewModeFactory: CategoryViewModelFactory
+    private lateinit var mealAdapter: MealAdapter
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -26,6 +33,7 @@ class CategoryDetail : Fragment(R.layout.activity_categories) {
         Log.d("Test str at Category", str)
         viewModeFactory = CategoryViewModelFactory(str)
         viewModel = ViewModelProvider(this, viewModeFactory).get(CategoryViewModel::class.java)
+        initAdapter()
         return view
     }
 
@@ -34,10 +42,35 @@ class CategoryDetail : Fragment(R.layout.activity_categories) {
 
 
         viewModel.categoryFoods.observe(viewLifecycleOwner) {
-             binding.tvCategoryCount.text = it.size.toString()
+            binding.tvCategoryCount.text =
+                 getString(R.string.tvCategoryName, viewModel.catName, it.size.toString())
+
 
         }
 
     }
+    private fun initAdapter() {
+        mealAdapter = MealAdapter(this)
+        binding.mealRecyclerview.apply {
+            adapter = mealAdapter
+            layoutManager = GridLayoutManager(context, 2, GridLayoutManager.VERTICAL, false)
+            viewModel.categoryFoods.observe(viewLifecycleOwner) {
+                (adapter as MealAdapter).differ.submitList(it)
+            }
+            setHasFixedSize(true)
+        }
+    }
+
+    override fun onItemClick(meal: MealX) {
+        Log.d("Test CategoryDetail", "onItemClick: ${meal.idMeal}")
+        meal.idMeal.let { viewModel.loadMealById(it) }
+        viewModel.mealById.observe(viewLifecycleOwner) {
+            val bundle = Bundle().apply {
+                putSerializable("meal", it[0])
+            }
+            findNavController().navigate(R.id.action_categoryDetail_to_mealDetail, bundle)
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/myfoods/Screens/CategoryDetail/CategoryViewModel.kt
+++ app/src/main/java/com/example/myfoods/Screens/CategoryDetail/CategoryViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import com.example.myfoods.Model.Meal
 import com.example.myfoods.Model.MealX
 import com.example.myfoods.Model.PopularMeal
+import com.example.myfoods.Model.RandomMeal
 import com.example.myfoods.Repository.MealRepo
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,9 +16,12 @@ import retrofit2.Response
 class CategoryViewModel(category : String) : ViewModel() {
     private val repo = MealRepo()
     private val _categoryFoods = MutableLiveData<List<MealX>>()
+    private val _mealById = MutableLiveData<List<Meal>>()
+    val mealById : LiveData<List<Meal>>
+        get() = _mealById
     val categoryFoods : LiveData<List<MealX>>
         get() = _categoryFoods
-    private val catName = category
+    val catName = category
 
     init {
         loadCategoryFoods()
@@ -34,6 +38,20 @@ class CategoryViewModel(category : String) : ViewModel() {
                 }
             }
             override fun onFailure(p0: Call<PopularMeal>, p1: Throwable) {
+                Log.d("TEST", p1.message.toString())
+            }
+        })
+    }
+    fun loadMealById(id: String) {
+        repo.getMealDetails(id).enqueue(object : Callback<RandomMeal> {
+            override fun onResponse(p0: Call<RandomMeal>, response: Response<RandomMeal>) {
+                if (response.body() != null) {
+                    _mealById.value = response.body()!!.meals
+                    Log.d("TEST id", "meal id ${_mealById.value}")
+                }
+            }
+
+            override fun onFailure(p0: Call<RandomMeal>, p1: Throwable) {
                 Log.d("TEST", p1.message.toString())
             }
         })
